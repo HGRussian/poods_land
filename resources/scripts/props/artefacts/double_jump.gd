4a0bc6e49c8130dfb801346327eae3b2b77f2068
mintyLeaf <mintyleaf_dev@gmail.com>
2017-08-08 00:51:32 +0300

    artefacts labels

--- resources/scripts/props/artefacts/double_jump.gd
+++ resources/scripts/props/artefacts/double_jump.gd
@@ -3,6 +3,7 @@ extends Sprite
 ### BASE CONFIG
 var tex = preload("res://resources/art/artefacts/double_jump.png")
 var art_name = "double_jump"
+var desc = "Extra jump \n Get another jump!"
 ### END
 
 var count = 0
@@ -30,9 +31,12 @@ func repeat():
 
 func _init():
 	texture = tex
+	$desc.text = desc
+	#
 
 func picked( who ):
 	var artefact_handler = who.get_node("artefact_handler")
+	$desc.rect_position.x = -$desc.rect_size.x/2
 	if artefact_handler.has_node(art_name):
 		artefact_handler.get_node(art_name).repeat()
 	else:
--- resources/scripts/props/artefacts/teleport.gd
+++ resources/scripts/props/artefacts/teleport.gd
@@ -3,6 +3,7 @@ extends Sprite
 ### BASE CONFIG
 var tex = preload("res://resources/art/artefacts/double_jump.png")
 var art_name = "teleport"
+var desc = "Fast teleport \n Double tap to teleport!"
 ### END
 
 var count = 0
@@ -62,9 +63,11 @@ func repeat():
 
 func _init():
 	texture = tex
+	$desc.text = desc
 
 func picked( who ):
 	var artefact_handler = who.get_node("artefact_handler")
+	$desc.rect_position.x = -$desc.rect_size.x/2
 	if artefact_handler.has_node(art_name):
 		artefact_handler.get_node(art_name).repeat()
 	else:
